735bd47588e4f4be288bd8e33c5d0e659b6750aa
Jawaid Aziz <135430447+javaid24@users.noreply.github.com>
2024-06-17 02:08:18 +0500

    commit

---
+++ src/Api/index.js
@@ -0,0 +1,8 @@
+const getPosts = async() => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "GET",
+    });
+    return response;
+}
+
+export default getPosts;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import getPosts from './Api';
 import './App.css';
 
 function App() {
+
+  useEffect( () => {
+    getPosts().then((posts) => console.log(posts))
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
