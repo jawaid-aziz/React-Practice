e0616572c6979853744918b110b25baba5d519a6
Jawaid Aziz <javaidmemon24@gmail.com>
2024-08-04 14:38:23 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import React, { useEffect } from "react";
-import Card from "./Components/Card";
-import Button from "./Components/Button";
-import Input from "./Components/input";
-import { useWeather } from "./Components/Context";
+import React from "react";
 
 function App() {
-  const weather = useWeather();
-  useEffect(() => {
-    weather.fetchLocation();
-  }, []);
 
   return (
     <div className="App" style={{display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column"}}>
-      <h1>Weather Forcast</h1>
-      <Input/>
-      <Button onClick={weather.fetchData} value="Search"/>
-      <Card/>
-      <Button value="Refresh"/>
+      <h1>React Practice</h1>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { WeatherProvider } from './Components/Context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <WeatherProvider>
         <App />
-        </WeatherProvider>
   </React.StrictMode>
 );
 
