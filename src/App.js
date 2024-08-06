87e7cdfac4315bcba90707e93cfa6e684245d8ad
Jawaid Aziz <javaidmemon24@gmail.com>
2024-08-06 23:07:04 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
-import { Profile } from "./Components/Profile";
-import { Counter } from "./Components/Counter";
+
 
 function App() {
 
   return (
     <div className="App" style={{display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column"}}>
       <h1>React Practice</h1>
-      <Profile/>
-      <Counter/>
+
     </div>
   );
 };
